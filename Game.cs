f50d73c0d636799a15204160e9b192d5a5980982
Vytenis <visak.pet0@gmail.com>
2017-01-22 22:54:48 +0200

    Added incomplete Game and Level Classes

---
+++ Game.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacman_DeepMind
+{
+    class Game
+    {
+        public Game()
+        {
+            GameStart();
+            GameLoop();
+            GameEnd();
+        }
+
+        private void GameStart()
+        {
+            Console.WriteLine("Pacman Game");
+
+            Level level = new Level("level1");
+        }
+
+        private void GameLoop()
+        {
+            var isWorking = true;
+            while(isWorking)
+            {
+
+
+                isWorking = false;
+            }
+        }
+
+        private void GameEnd()
+        {
+            Console.WriteLine("Game Over!");
+        }
+    }
+}
---
+++ Level.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacman_DeepMind
+{
+    class Level
+    {
+        private string _name;
+        private char[,] _board;
+        //private int _maxScore;
+
+        public Level(string name)
+        {
+            _name = "levels/" + name + ".txt";
+
+            _GetLevelInfo();
+        }
+
+
+
+        private void _GetLevelInfo()
+        {
+            if(File.Exists(_name))
+            {
+                TextReader data = File.OpenText(_name);
+                string levelSize = data.ReadLine();
+                string[] coords = levelSize.Split();
+                int x = int.Parse(coords[0]);
+                int y = int.Parse(coords[1]);
+                //Console.WriteLine(x + "\t" + y);
+                
+                _board = new char[x, y];
+
+                string line = File.ReadAllText(_name);
+                Console.WriteLine("\n" + line + "\n");
+
+
+            }
+            else
+            {
+                Console.WriteLine("Error! Level file doesn't exist!");
+            }
+        }
+    }
+}
