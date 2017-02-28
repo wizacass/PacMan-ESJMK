fefce9457a34e3e51c0ad39eea1352770bafbdd8
Vytenis <visak.pet0@gmail.com>
2017-02-28 16:22:23 +0200

    Added Node.cs

---
+++ Node.cs
@@ -0,0 +1,19 @@
+﻿namespace Pacman_DeepMind
+{
+    using Genetic;
+    using System.Collections.Generic;
+
+    class Node
+    {
+        int x, y;   //Coords
+        float G;    //distance from previous
+        float H;    //distance from start
+        float F;    //sum of G + H
+
+        NodeState State;
+        Node ParentNode; //previous Node
+        List<Directions> Adjacent;
+    }
+
+    enum NodeState { Untested, Open, Closed }
+}
